c2036cd6637189b89ce619134f19ce488093176b
Juani <nacho.mone@gmail.com>
2019-10-26 18:26:40 -0300

    Alta y actualizacion servicio usuario

--- TPFINALPPS/src/app/clases/supervisor.ts
+++ TPFINALPPS/src/app/clases/supervisor.ts
@@ -1,14 +1,23 @@
 import { Usuario } from './usuario';
 
 export class Supervisor extends Usuario {
+  public cuil: string;
+  public perfil: string;
 
-    public cuil : string;
-    public perfil : string;
-
-    constructor(nombre: string, apellido: string, dni: string, foto: string, activo: boolean, email: string,
-        rol: string, id: string, cuil: string, perfil: string){
-        super(nombre, apellido, dni, foto, activo, email, rol, id);
-        this.cuil = cuil;
-        this.perfil=perfil;
-    }
+  constructor(
+    nombre: string,
+    apellido: string,
+    dni: string,
+    foto: string,
+    activo: boolean,
+    email: string,
+    rol: string,
+    id: string,
+    cuil: string,
+    perfil: string
+  ) {
+    super(nombre, apellido, dni, foto, activo, email, rol, id);
+    this.cuil = cuil;
+    this.perfil = perfil;
+  }
 }
