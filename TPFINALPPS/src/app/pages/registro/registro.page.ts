ecdca5686bfbffb09122e38d5a9becf3e620e238
fernandeztrevor <fernandeztrevor@outlook.com>
2019-10-29 14:29:46 -0300

    pagina de registro lista, falta validar y enviar

--- TPFINALPPS/src/app/pages/registro/registro.page.ts
+++ TPFINALPPS/src/app/pages/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/servicios/auth.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  public habilitarCuil:boolean=false;
+  public habilitarTipoDeEmpleado:boolean=false;
+  public habilitarPerfil:boolean=false;
+  public ocultar:boolean=true;
+
+  public nombre:string;
+  public apellido:string;
+  public dni:string;
+  public email:string;
+  public cuil:string;
+  public tipoDeEmpleado:string;
+  public perfil:string;
+
+  public password1:string;
+  public password2:string;
+
+  
+  constructor(private authService: AuthService) {
+
+    
+   }
 
   ngOnInit() {
+  }
+
+  rbInputs(event){
+    console.log(event.detail.value);
+    this.ocultar =false;
+    switch(event.detail.value){
+      case "cliente":
+        this.habilitarCuil=true;
+        this.habilitarTipoDeEmpleado=true;
+        this.habilitarPerfil=true;
+        break;
+      case "empleado":
+        this.habilitarCuil=false;
+        this.habilitarTipoDeEmpleado=false;
+        this.habilitarPerfil=true;
+        break;
+      case "supervisor":
+          this.habilitarCuil=false;
+          this.habilitarTipoDeEmpleado=true;
+          this.habilitarPerfil=false;
+        break;
+    }
+  }
+
+  registrarse() {
+    //this.authService.SignUp(this.email, this.clave);
+    console.log("lo mando a registrar");
   }
 
 }
