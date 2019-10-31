97696234518a257c940f7992d64af6ea0a0698a8
fernandeztrevor <fernandeztrevor@outlook.com>
2019-10-31 11:43:06 -0300

    sigue sin funcionar sacar foto

--- TPFINALPPS/src/app/componentes/sacar-foto/sacar-foto.component.ts
+++ TPFINALPPS/src/app/componentes/sacar-foto/sacar-foto.component.ts
@@ -18,42 +18,42 @@ export class SacarFotoComponent implements OnInit {
 
   images = [];
 
-  constructor(private camera:Camera, private file:File, private http:HttpClient, private webView:WebView,
-    private actionSheetController:ActionSheetController, private toastController:ToastController,
-    private storage: Storage, private plt:Platform, private loadingController: LoadingController,
-    private ref: ChangeDetectorRef ) { }
+  constructor(private camera: Camera, private file: File, private http: HttpClient, private webView: WebView,
+    private actionSheetController: ActionSheetController, private toastController: ToastController,
+    private storage: Storage, private plt: Platform, private loadingController: LoadingController,
+    private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
-    this.plt.ready().then(()=>{
+    this.plt.ready().then(() => {
       this.loadStoredImages();
     });
   }
 
 
-  loadStoredImages(){
+  loadStoredImages() {
     this.storage.get(STORAGE_KEY).then(images => {
-      if(images){
+      if (images) {
         let arr = JSON.parse(images);
         this.images = [];
-        for(let img of arr){
+        for (let img of arr) {
           let filePath = this.file.dataDirectory + img;
           let resPath = this.pathForImage(filePath);
-          this.images.push({ name: img, path: resPath, filePath: filePath});
+          this.images.push({ name: img, path: resPath, filePath: filePath });
         }
       }
     });
   }
 
-  pathForImage(img){
-    if(img === null){
+  pathForImage(img) {
+    if (img === null) {
       return '';
-    }else{
+    } else {
       let converted = this.webView.convertFileSrc(img);
       return converted;
     }
   }
 
-  async presentToast(text){
+  async presentToast(text) {
     const toast = await this.toastController.create({
       message: text,
       position: 'bottom',
@@ -62,7 +62,7 @@ export class SacarFotoComponent implements OnInit {
     toast.present();
   }
 
-  async selectedImage(){
+  async selectedImage() {
     const actionSheet = await this.actionSheetController.create({
       header: "Seleccione el origen de la imagen",
       buttons: [{
@@ -77,17 +77,17 @@ export class SacarFotoComponent implements OnInit {
           this.takePicture(this.camera.PictureSourceType.CAMERA);
         }
       },
-    {
-      text: 'Cancelar',
-      role: 'cancel'
-    }
-  ]
+      {
+        text: 'Cancelar',
+        role: 'cancel'
+      }
+      ]
     });
     await actionSheet.present();
   }
 
-  takePicture(sourceType: PictureSourceType){
-    var options: CameraOptions ={
+  takePicture(sourceType: PictureSourceType) {
+    var options: CameraOptions = {
       quality: 100,
       sourceType: sourceType,
       saveToPhotoAlbum: false,
@@ -95,33 +95,34 @@ export class SacarFotoComponent implements OnInit {
     };
 
     this.camera.getPicture(options).then(imagePath => {
-      var currentName = imagePath.substr(imagePath.lastIndexOf('/')+1);
-      var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/')+1);
-      alert(correctPath);
+      var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+      var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
       this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
     });
   }
 
-  copyFileToLocalDir(namePath, currentName, newFileName){
-    this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(_ =>{
+  copyFileToLocalDir(namePath, currentName, newFileName) {
+
+    
+    this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(_ => {
       this.updateStoredImages(newFileName);
     }, error => {
-      this.presentToast('Error mientras se guardaba la foto');
+      this.presentToast('Error mientras se guardaba la foto'+error);
     });
   }
 
-  createFileName(){
+  createFileName() {
     var d = new Date(),
-    n=d.getTime(),
-    newFileName = n + ".jpg";
+      n = d.getTime(),
+      newFileName = n + ".jpg";
     return newFileName;
   }
 
 
-  updateStoredImages(name){
+  updateStoredImages(name) {
     this.storage.get(STORAGE_KEY).then(images => {
       let arr = JSON.parse(images);
-      if(!arr){
+      if (!arr) {
         let newImages = [name];
         this.storage.set(STORAGE_KEY, JSON.stringify(newImages));
       } else {
@@ -142,14 +143,14 @@ export class SacarFotoComponent implements OnInit {
     });
   }
 
-  deleteImage(imgEntry, position){
+  deleteImage(imgEntry, position) {
     this.images.splice(position, 1);
     this.storage.get(STORAGE_KEY).then(images => {
-      let arr= JSON.parse(images);
+      let arr = JSON.parse(images);
       let filtered = arr.filter(name => name != imgEntry.name);
       this.storage.set(STORAGE_KEY, JSON.stringify(filtered));
 
-      var correctPath = imgEntry.filePath.substr(0, imgEntry.filePath.lastindexOf('/')+1);
+      var correctPath = imgEntry.filePath.substr(0, imgEntry.filePath.lastindexOf('/') + 1);
 
       this.file.removeFile(correctPath, imgEntry.name).then(res => {
         this.presentToast('Archivo borrado');
@@ -157,10 +158,10 @@ export class SacarFotoComponent implements OnInit {
     });
   }
 
- startUpload(imgEntry){
-  this.file.resolveLocalFilesystemUrl(imgEntry.filePath)
-  .then(entry => {
-    alert("culo");
-  })
- }
+  startUpload(imgEntry) {
+    this.file.resolveLocalFilesystemUrl(imgEntry.filePath)
+      .then(entry => {
+        alert("culo");
+      })
+  }
 }
