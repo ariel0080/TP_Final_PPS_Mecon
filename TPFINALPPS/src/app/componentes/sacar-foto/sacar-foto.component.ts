4c5308f3805f2aaf552487261b89c2880c8f2309
fernandeztrevor <fernandeztrevor@outlook.com>
2019-11-01 18:23:21 -0300

    componente sacar foto semi funcionando

--- TPFINALPPS/src/app/componentes/sacar-foto/sacar-foto.component.ts
+++ TPFINALPPS/src/app/componentes/sacar-foto/sacar-foto.component.ts
@@ -1,165 +1,63 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { Camera, PictureSourceType, CameraOptions } from '@ionic-native/camera/ngx';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
-import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
-import { Storage } from '@ionic/storage';
-import { File } from '@ionic-native/file/ngx';
 
 
-const STORAGE_KEY = 'my_images';
 @Component({
   selector: 'app-sacar-foto',
   templateUrl: './sacar-foto.component.html',
   styleUrls: ['./sacar-foto.component.scss'],
 })
 export class SacarFotoComponent implements OnInit {
+  image: string;
 
-
-  images = [];
-
-  constructor(private camera:Camera, private file:File, private http:HttpClient, private webView:WebView,
-    private actionSheetController:ActionSheetController, private toastController:ToastController,
-    private storage: Storage, private plt:Platform, private loadingController: LoadingController,
-    private ref: ChangeDetectorRef ) { }
+  constructor(private camera: Camera, private webView: WebView) { }
 
   ngOnInit() {
-    this.plt.ready().then(()=>{
-      this.loadStoredImages();
-    });
+
   }
 
-
-  loadStoredImages(){
-    this.storage.get(STORAGE_KEY).then(images=> {
-      if(images){
-        let arr = JSON.parse(images);
-        this.images = [];
-        for(let img of arr){
-          let filePath = this.file.dataDirectory + img;
-          let resPath = this.pathForImage(filePath);
-          this.images.push({ name: img, path: resPath, filePath: filePath});
-        }
-      }
-    });
-  }
-
-  pathForImage(img){
-    if(img === null){
-      return '';
-    }else{
-      let converted = this.webView.convertFileSrc(img);
-      return converted;
-    }
-  }
-
-  async presentToast(text){
-    const toast = await this.toastController.create({
-      message: text,
-      position: 'bottom',
-      duration: 3000
-    });
-    toast.present();
-  }
-
-  async selectedImage(){
-    const actionSheet = await this.actionSheetController.create({
-      header: "Select Image Source",
-      buttons: [{
-        text: 'Load from Library',
-        handler: () => {
-          this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
-        }
-      },
-      {
-        text: 'Use Camera',
-        handler: () => {
-          this.takePicture(this.camera.PictureSourceType.CAMERA);
-        }
-      },
-    {
-      text: 'Cancel',
-      role: 'cancel'
-    }
-  ]
-    });
-    await actionSheet.present();
-  }
-
-  takePicture(sourceType: PictureSourceType){
-    var options: CameraOptions ={
-      quality: 100,
-      sourceType: sourceType,
-      saveToPhotoAlbum: false,
-      correctOrientation: true
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 50,
+      targetHeight: 400,
+      targetWidth: 400,
+      //destinationType: this.camera.DestinationType.DATA_URL,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      //sourceType: this.camera.PictureSourceType.CAMERA
     };
-
-    this.camera.getPicture(options).then(imagePath => {
-      var currentName = imagePath.substr(imagePath.lastIndexOf('/')+1);
-      var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/')+1);
-      this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+    console.log("entro");
+    this.camera.getPicture(options).then((ImageData) => {
+      //this.image = 'data:image/jpeg;base64,' + ImageData;
+      
+      this.image = this.webView.convertFileSrc(ImageData);
+      console.log(this.image);
+    }, (err) => {
+      console.log(err);
     });
+
+		/*	const options: CameraOptions = {
+				quality: 100,
+				targetHeight: 400,
+				targetWidth: 400,
+				destinationType: this.camera.DestinationType.DATA_URL,
+				encodingType: this.camera.EncodingType.JPEG,
+				mediaType: this.camera.MediaType.PICTURE,
+				correctOrientation: true,
+				allowEdit: true,
+			}
+
+			this.camera.getPicture(options).then((imageData) => {
+        //let base64Image = 'data:image/jpeg;base64,' + imageData;
+        this.image = this.webView.convertFileSrc(imageData);
+			//	this.image = base64Image;
+			}).catch((err) => {
+				console.log("Error: ", err);
+			});*/
+
+
   }
 
-  copyFileToLocalDir(namePath, currentName, newFileName){
-    this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(_ =>{
-      this.updateStoredImages(newFileName);
-    }, error => {
-      this.presentToast('Error mientras se guardaba la foto');
-    });
-  }
-
-  createFileName(){
-    var d = new Date(),
-    n=d.getTime(),
-    newFileName = n + ".jpg";
-    return newFileName;
-  }
-
-
-  updateStoredImages(name){
-    this.storage.get(STORAGE_KEY).then(images => {
-      let arr = JSON.parse(images);
-      if(!arr){
-        let newImages = [name];
-        this.storage.set(STORAGE_KEY, JSON.stringify(newImages));
-      } else {
-        arr.push(name);
-        this.storage.set(STORAGE_KEY, JSON.stringify(arr));
-      }
-      let filePath = this.file.dataDirectory + name;
-      let resPath = this.pathForImage(filePath);
-
-      let newEntry = {
-        name: name,
-        path: resPath,
-        filePath: filePath
-      };
-
-      this.images = [newEntry, ...this.images];
-      this.ref.detectChanges();
-    });
-  }
-
-  deleteImage(imgEntry, position){
-    this.images.splice(position, 1);
-    this.storage.get(STORAGE_KEY).then(images => {
-      let arr= JSON.parse(images);
-      let filtered = arr.filter(name => name != imgEntry.name);
-      this.storage.set(STORAGE_KEY, JSON.stringify(filtered));
-
-      var correctPath = imgEntry.filePath.substr(0, imgEntry.filePath.lastindexOf('/')+1);
-
-      this.file.removeFile(correctPath, imgEntry.name).then(res => {
-        this.presentToast('Archivo borrado');
-      });
-    });
-  }
-
- startUpload(imgEntry){
-  this.file.resolveLocalFilesystemUrl(imgEntry.filePath)
-  .then(entry => {
-    alert("culo");
-  })
- }
 }
